5d86622e851875f50da204f1c595107869dfd2c8
Sergey Makridenkov <sergey@makridenkov.com>
2014-03-31 12:41:37 +0200

    Make middleware definition less complex

---
+++ spec/features/registration_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe 'Registration', :type => :feature, feature: true, js: true  do
+  let(:email){ "sergey#{rnd}@makridenkov.com" }
+
+  context 'when invalid data' do
+    it 'shows errors' do
+      # visit registration
+      # click on 'registration'
+      # I should see an errors
+
+      visit '/registration'
+      click_on 'Registration'
+
+      expect(page).to have_content("Email is required")
+    end
+  end
+
+  context 'when valid data' do
+    it 'register an user'
+      # Given I am on registration page
+      # Wnen I fill the form
+      # And click on 'registration'
+      # Then I should be on login page
+      # And see message prease check email for password
+      # And I should get an Email
+  end
+end
